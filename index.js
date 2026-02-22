5c14f86c6b9aad3f7cb1e472ab15a688f113f83e
akash-yadav08 <akashyadav9259@gmail.com>
2026-02-22 13:54:50 +0530

    Nutrition1-app

--- index.js
+++ index.js
@@ -4,25 +4,13 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-// Mongo
-mongoose.connect("mongodb://localhost:27017/nutrition")
+mongoose.connect("mongodb://mongo:27017/nutrition")
 .then(()=>console.log("Mongo connected"))
 .catch(err=>console.log(err));
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-// Schema
-const userSchema = new mongoose.Schema({
-    height:Number,
-    weight:Number,
-    bmi:Number,
-    calories:Number,
-    diet:String
-});
-const User = mongoose.model("User", userSchema);
-
-// Logic
 function calculate(height,weight){
     let h=height/100;
     let bmi=weight/(h*h);
@@ -36,19 +24,11 @@ function calculate(height,weight){
     return {bmi,calories,diet};
 }
 
-// API
-app.post("/submit", async (req,res)=>{
+app.post("/submit",(req,res)=>{
     let {height,weight}=req.body;
     let r=calculate(Number(height),Number(weight));
-
-    await User.create({
-        height,weight,
-        bmi:r.bmi,
-        calories:r.calories,
-        diet:r.diet
-    });
 
     res.json({bmi:r.bmi.toFixed(2),cal:r.calories,diet:r.diet});
 });
 
-app.listen(3000,()=>console.log("Server running on 3000"));
+app.listen(3000,()=>console.log("Server running"));
